2e059190ce5d7c2cb1373f1eb7865476eb7b42d7
Stephen <swrobbin@gmail.com>
2021-09-24 15:33:53 -0400

    Done.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+const [liClass, setLiClass] = useState('')
+
+function clickHandler(){
+  liClass === '' ? setLiClass("in-cart") : setLiClass('')
+
+}
+
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button  onClick={clickHandler} className="add">{liClass === '' ? 'Add to Cart' : "Remove From Cart"}</button>
     </li>
   );
 }
